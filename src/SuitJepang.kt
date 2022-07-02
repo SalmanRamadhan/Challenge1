ea498c7ce50ec5f93e6bc6480ec92ed0ee015ac8
RodanDuamar <muhammadrodan9@gmail.com>
2022-07-02 09:23:47 +0700

    Progres version 1

--- src/SuitJepang.kt
+++ src/SuitJepang.kt
@@ -1,7 +1,20 @@
 class SuitJepang (
     var pemain1: String,
     var pemain2: String
-        ){
-
-
+        ) {
+    fun suit(pemain1: String, pemain2: String) {
+        if (pemain1 == pemain2) {
+            println("Draw")
+        }
+        if (pemain1 == "gunting" && pemain2 == "batu") {
+            println("pemain2 menang")
+        }
+        if (pemain1 == "gunting" && pemain2 == "kertas") {
+            println("pemain1 menang")
+        }
+        if (pemain1 == "batu" && pemain2 == "kertas") {
+            println("pemain1 menang")
+        }
+    }
 }
+
