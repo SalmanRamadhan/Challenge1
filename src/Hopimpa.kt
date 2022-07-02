d69166ef387b27072787425acffa2671522769c8
SalmanRamadhan <msalmanrama98@gmail.com>
2022-07-02 08:50:38 +0700

    Membuat constructor

---
+++ src/Hopimpa.kt
@@ -0,0 +1,16 @@
+class Hopimpa(
+    var pemain1: String,
+    var pemain2: String,
+    var pemain3: String
+) {
+
+    fun play(pemain1: String,pemain2: String,pemain3: String) {
+
+        
+
+    }
+
+
+
+
+}
