6320e1f207cf2ecc0d06b012c88e348b665bf2bc
SalmanRamadhan <msalmanrama98@gmail.com>
2022-07-02 08:51:56 +0700

    Version 1

--- src/Hopimpa.kt
+++ src/Hopimpa.kt
@@ -6,7 +6,26 @@ class Hopimpa(
 
     fun play(pemain1: String,pemain2: String,pemain3: String) {
 
-        
+        var counterHitam = 0
+        var counterPutih = 0
+
+        val arraypemain = arrayOf(pemain1,pemain2,pemain3)
+
+        for(item in arraypemain){
+            if(item == "hitam"){
+                counterHitam+=1
+            } else {
+                counterPutih+=1
+            }
+        }
+
+        if(counterPutih < counterHitam){
+            println("pemain putih keluar")
+        } else if(counterHitam < counterPutih){
+            println("pemain hitam keluar")
+        } else {
+            println("draw")
+        }
 
     }
 
